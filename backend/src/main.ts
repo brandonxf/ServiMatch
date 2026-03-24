d86d69105c3ee005ee000b30d606644d8d0a394b
brandonxf <brandonace07@gmail.com>
2026-03-24 01:48:21 -0500

    chore(debug): add verbose logging to debug startup hang

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,13 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  console.log('[DEBUG] Iniciando bootstrap...');
+  console.log('[DEBUG] Ejecutando NestFactory.create...');
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+    abortOnError: false // ensures it prints error if it crashes
+  });
+  console.log('[DEBUG] NestFactory finalizado con éxito.');
   const config = app.get(ConfigService);
 
   app.use(helmet());
